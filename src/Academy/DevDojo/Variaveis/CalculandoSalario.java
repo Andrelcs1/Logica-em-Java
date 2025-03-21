448bf5808eed783517e4087a9db0b53a7d9da299
André Santos <andresantos@Andres-MacBook-Pro.local>
2025-03-21 11:09:30 -0300

    adicionando mensagem

--- src/Academy/DevDojo/Variaveis/CalculandoSalario.java
+++ src/Academy/DevDojo/Variaveis/CalculandoSalario.java
@@ -9,5 +9,6 @@ public class CalculandoSalario {
        System.out.println("15% of the wage is= " + result);
        result = wage * 0.05f;
        System.out.println("5% of the wage is= " + result);
+       System.out.println("Esses foram os calculos :D");
     }
 }
